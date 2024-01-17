c96e0247061dae129a2d8e908f0dde9bdb9f867b
Iosif Chatzimichail <afonic@gmail.com>
2024-01-18 00:51:45 +0200

    Change assets location and build options

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,21 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: `[name].js`,
+                chunkFileNames: `[name].js`,
+                assetFileNames: `[name].[ext]`
+            }
+        },
+        manifest: false
+    },
     plugins: [
         laravel({
             hotFile: "vite.hot",
             publicDirectory: "resources",
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/slf-styles.css', 'resources/js/slf-scripts.js'],
             refresh: true,
         }),
     ],
