24e7ac6444197069059528adc68298c1a1e64279
Iosif Chatzimichail <afonic@users.noreply.github.com>
2024-05-13 12:21:47 +0300

    Another take

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,6 +5,7 @@ namespace Reach\StatamicLivewireFilters\Tests;
 use Statamic\Extend\Manifest;
 use Statamic\Facades\File;
 use Statamic\Facades\YAML;
+use Statamic\Facades\Site;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
@@ -22,10 +23,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
             $this->preventSavingStacheItemsToDisk();
         }
 
-        dd(resource_path('sites.yaml'));
-
-        // We changed the default sites setup but the tests assume defaults like the following.
-        File::put(resource_path('sites.yaml'), YAML::dump([
+        File::put(resource_path('sites.yaml'), YAML::dump($sites = [
             'en' => [
                 'name' => 'English',
                 'url' => 'http://localhost/',
@@ -33,6 +31,8 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
                 'lang' => 'en',
             ],
         ]));
+        
+        Site::setSites($sites);
     }
 
     public function tearDown(): void
