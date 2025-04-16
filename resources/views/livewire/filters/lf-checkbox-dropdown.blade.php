be449d30682a54fd4943d3a0656d9e2d290ae181
Iosif Chatzimichail <afonic@users.noreply.github.com>
2025-04-16 12:33:56 +0300

    Not needed

--- resources/views/livewire/filters/lf-checkbox-dropdown.blade.php
+++ resources/views/livewire/filters/lf-checkbox-dropdown.blade.php
@@ -76,9 +76,6 @@
                             value="{{ $value }}" 
                             id="checkboxOption{{ $loop->index }}"
                         />
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-width="4" class="pointer-events-none invisible absolute left-1/2 top-1/2 size-3 -translate-x-1/2 -translate-y-1/2 text-neutral-100 peer-checked:visible dark:text-black" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"/>
-                        </svg>
                     </div>
                     <span>
                         {{ $label }}
