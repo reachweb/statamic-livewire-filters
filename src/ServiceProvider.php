a004ce8e68a9c3f403bdc299c2fdee880a7b0c85
Iosif Chatzimichail <afonic@users.noreply.github.com>
2025-12-26 23:46:43 +0200

    Fixes #58

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Reach\StatamicLivewireFilters;
 
-use Illuminate\Support\Facades\Artisan;
 use Livewire\Livewire;
 use Reach\StatamicLivewireFilters\Http\Livewire\LfCheckboxFilter;
 use Reach\StatamicLivewireFilters\Http\Livewire\LfCount;
@@ -77,9 +76,5 @@ class ServiceProvider extends AddonServiceProvider
         $this->publishes([
             __DIR__.'/../config/config.php' => config_path('statamic-livewire-filters.php'),
         ], 'statamic-livewire-filters-config');
-
-        if ($this->app->runningInConsole()) {
-            Artisan::call('statamic-livewire-filters:update');
-        }
     }
 }
